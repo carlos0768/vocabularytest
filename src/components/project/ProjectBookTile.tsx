a1a178b4e97667b66d7319187c0cf176a6fd0159
carlos0768 <carlotti1208@gmail.com>
2026-02-18 22:39:46 +0900

    Fix OCR API call parameter

--- src/components/project/ProjectBookTile.tsx
+++ src/components/project/ProjectBookTile.tsx
@@ -120,6 +120,7 @@ export function ProjectBookTile({
 
       <Link
         href={`/project/${project.id}`}
+        prefetch={false}
         className={`block rounded-xl border-2 border-b-4 bg-[var(--color-surface)] p-3 pb-2.5 active:border-b-2 active:mt-[2px] transition-all ${
           hasProgress
             ? 'border-[var(--color-success)]'
